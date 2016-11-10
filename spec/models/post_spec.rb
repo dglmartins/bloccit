39d2c9d34a5a298bfff7c3b0912f387c6e86afa9
danilomartins <dglmartins@outlook.com>
2016-11-10 18:49:05 +0800

    assignment 30 voting completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -82,8 +82,23 @@ RSpec.describe Post, type: :model do
          expect(post.rank).to eq (old_rank - 1)
        end
      end
+  end
 
+  describe "upvote after_create" do
+    it "upvotes the post after create" do
+      expect(post.up_votes).to eq(1)
+    end
 
+    it "calls create_vote when post is created" do
+      post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+      expect(post).to receive(:create_vote)
+      post.save
+    end
+
+    it "associates the vote with owner" do
+      expect(post.votes.first.user).to eq(post.user)
+    end
 
   end
+
 end
